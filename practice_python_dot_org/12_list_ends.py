2727dac167c8438e5537dd5fcf614863fe030331
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-04-01 13:56:24 -0300

    Complete exercise 12

---
+++ practice_python_dot_org/12_list_ends.py
@@ -0,0 +1,9 @@
+#Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+
+a = [5, 10, 15, 20, 25, 36]
+
+def list_ends(list):
+    print(a[0], a[-1])
+
+list_ends(a)
+

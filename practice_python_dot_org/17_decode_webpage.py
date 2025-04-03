b16d310f9e14225962a2876a69f72db70e53fbe7
Morellicode <110130671+MorelliCode@users.noreply.github.com>
2025-04-03 11:09:43 -0300

    Start exercise 17

---
+++ practice_python_dot_org/17_decode_webpage.py
@@ -0,0 +1,11 @@
+# Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage.
+
+import requests
+from bs4 import BeautifulSoup 
+
+url = "http://www.nytimes.com"
+r = requests.get(url)
+r_html = r.text
+soup = BeautifulSoup(r_html)
+
+print(soup.findAll('p'))

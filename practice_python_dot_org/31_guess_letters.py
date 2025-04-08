6e879722431e7f64405e379cf155e6c83fc58360
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2025-04-08 18:08:12 -0300

    Start exercise 31

---
+++ practice_python_dot_org/31_guess_letters.py
@@ -0,0 +1,42 @@
+#  This exercise is Part 2 of 3 of the Hangman exercise series. The other exercises are: Part 1 and Part 3.
+
+# Let’s continue building Hangman. In the game of Hangman, a clue word is given by the program that the player has to guess, letter by letter. The player guesses one letter at a time until the entire word has been guessed. (In the actual game, the player can only guess 6 letters incorrectly before losing).
+
+# Let’s say the word the player has to guess is “EVAPORATE”. For this exercise, write the logic that asks a player to guess a letter and displays letters in the clue word that were guessed correctly. For now, let the player guess an infinite number of times until they get the entire word. As a bonus, keep track of the letters the player guessed and display a different message if the player tries to guess that letter again. Remember to stop the game when all the letters have been guessed correctly! Don’t worry about choosing a word randomly or keeping track of the number of guesses the player has remaining - we will deal with those in a future exercise.
+
+# An example interaction can look like this:
+
+# >>> Welcome to Hangman!
+# _ _ _ _ _ _ _ _ _
+# >>> Guess your letter: S
+# Incorrect!
+# >>> Guess your letter: E
+# E _ _ _ _ _ _ _ E
+# ...
+
+# And so on, until the player gets the word.
+
+chosen_word = "EVAPORATE"
+
+to_guess_lines = []
+letters_guessed = []
+
+def guess_lines(word):
+    to_guess = [[],[]]
+    for letter in word:
+        to_guess[0].append("_")
+        to_guess[1].append(letter)
+    return  to_guess
+
+def display_game(list_of_lists):
+    print(" ".join(list_of_lists[0]))
+    print("You've already guessed: " + " ".join(letters_guessed)) 
+    
+def ask_for_guess():
+    
+    
+
+to_guess_lines = guess_lines(chosen_word)
+print(to_guess_lines)
+display_game(to_guess_lines)
+#

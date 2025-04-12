2b740f7e78686f22012b311021b9f1c15e1d1205
Morellicode <110130671+MorelliCode@users.noreply.github.com>
2025-04-12 18:27:54 -0300

    Complete day 2 puzzle 1

--- advent_of_code/day_2_1.py
+++ advent_of_code/day_2_1.py
@@ -27,8 +27,39 @@
 
 # Analyze the unusual data from the engineers. How many reports are safe?
 
+is_valid = False
+des_asc = []
+number_safe = 0
+
 with open("day_2.txt", "r") as f:
     line = f.readline().strip().split()
     while line:
         for i in range(len(line)):
-            
+            line[i] = int(line[i])
+        is_valid = False
+        des_asc = []
+        for i in range(len(line)):
+            if i == len(line) - 1:
+                break
+            if abs(line[i] - line[i+1]) > 3:
+                is_valid = False
+                break
+            else:
+                is_valid = True
+                if line[i] > line[i+1]:
+                    des_asc.append(-1)
+                elif line[i] < line[i+1]:
+                    des_asc.append(1)
+                else:
+                    is_valid = False
+                    break
+
+        if is_valid == True:
+            if -1 in des_asc and 1 in des_asc:
+                line = f.readline().strip().split()
+                continue
+            else:
+                number_safe +=1
+        line = f.readline().strip().split()
+
+print(number_safe)

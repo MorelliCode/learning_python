fb865ec914ab8a42f0b09d302a90d05c1dcbc829
Morellicode <110130671+MorelliCode@users.noreply.github.com>
2025-04-13 10:37:44 -0300

    Complete day 3 puzzle 2

--- advent_of_code/day_3.py
+++ advent_of_code/day_3.py
@@ -16,9 +16,14 @@
 import re
 
 
-def find_patterns(string_to_process):
+def find_patterns_part_one(string_to_process):
     #Use regular expressions to extract a list of all "mul(X,Y)" values (where X and Y are 1 to 3 digits numbers)
     processed_list = re.findall(r'mul\(\d{1,3}\,\d{1,3}\)', string_to_process)
+    return processed_list
+
+def find_patterns_part_two(string_to_process):
+    #Use regular expressions to extract a list of all "mul(X,Y)" values (where X and Y are 1 to 3 digits numbers)
+    processed_list = re.findall(r"mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don\'t\(\)", string_to_process)
     return processed_list
 
 def execute_multiplication(pattern):
@@ -36,10 +41,32 @@ def part_one():
         added_multiplications = 0
         text = f.readlines()
         text = "".join(text)
-        patterns = find_patterns(text)
+        patterns = find_patterns_part_one(text)
         for item in patterns:
             added_multiplications += execute_multiplication(item)
         print(added_multiplications)
 
 
+def part_two():
+    with open("day_3.txt", "r") as f:
+        added_multiplications = 0
+        text = f.readlines()
+        text = "".join(text)
+        patterns = find_patterns_part_two(text)
+        do_or_not = True
+        for item in patterns:
+            if item == "do()":
+                do_or_not = True
+                continue
+            elif item == "don't()":
+                do_or_not = False
+                continue
+            if do_or_not == False:
+                continue
+            else:
+                added_multiplications += execute_multiplication(item)
+        print(added_multiplications)
+
 part_one()
+
+part_two()
